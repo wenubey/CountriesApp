c9d94301cb0e3411a456128930417ceb5a543d36
wenubey <mertfatihsimsek06@gmail.com>
2023-04-21 22:32:53 +0200

    create CountriesInContinent.graphql query to given continent name get data to related create ContinentMap.kt create GetCountriesInContinentUseCase.kt UseCase add Mappers.kt to countriesincontinent.tosimplycountry mapper function

---
+++ app/src/main/java/com/wenubey/countriesapp/core/ContinentMap.kt
@@ -0,0 +1,15 @@
+package com.wenubey.countriesapp.core
+
+object ContinentMap {
+    val continentMap = mapOf(
+        "AF" to "Africa",
+        "AN" to "Antarctica",
+        "AS" to "Asia",
+        "EU" to "Europe",
+        "NA" to "North America",
+        "OC" to "Oceania",
+        "SA" to "South America"
+    )
+
+}
+
