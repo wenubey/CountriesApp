f9920eef82b76efe7ae169a7c75c2aacb3cbdb58
wenubey <mertfatihsimsek06@gmail.com>
2023-04-22 21:44:18 +0200

    improving ui and add some components

--- app/src/main/java/com/wenubey/countriesapp/presentation/CountriesScreen.kt
+++ app/src/main/java/com/wenubey/countriesapp/presentation/CountriesScreen.kt
@@ -1,6 +1,5 @@
 package com.wenubey.countriesapp.presentation
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,10 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,13 +18,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.wenubey.countriesapp.R
-import com.wenubey.countriesapp.core.ContinentMap
 import com.wenubey.countriesapp.core.components.ProgressBar
 import com.wenubey.countriesapp.presentation.components.CountrySelectRow
 import com.wenubey.countriesapp.presentation.components.NumberSelectRow
